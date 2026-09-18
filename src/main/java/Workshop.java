4a89d9ea321339e9b46e8d568d1e9de85432e464
Juan67 <juanstan08@gmail.com>
2026-09-17 20:39:23 -0500

    MetodoFactorial

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -33,12 +33,23 @@ public class Workshop {
         return resultado;
     }
 
-    // Método que calcula el factorial de un número entero
+  // Método que calcula el factorial de un número entero
     public int factorial(int n) {
         // TODO: Implementar el método para calcular el factorial de un número entero.
         // Ejemplo: Si n = 5, el resultado debería ser 120.
         // Lanzar IllegalArgumentException si n es negativo.
-        return 0;
+        if (n < 0) {
+     throw new IllegalArgumentException("El número no puede ser negativo");
+}
+
+     int resultado = 1;
+
+     for (int i = 1; i <= n; i++) {
+       resultado *= i;
+}
+
+      return resultado;
+
     }
 
     // Método que verifica si un número es primo
