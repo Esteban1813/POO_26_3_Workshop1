fa03d8e9314e8494de19d291a35124a3384ade9c
Juan67 <juanstan08@gmail.com>
2026-09-17 23:44:33 -0500

    CuentaYinviertecadena

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -272,28 +272,31 @@ public int[] rotarArreglo(int[] arreglo, int posiciones) {
     return resultado;
 }
 
-          // Método que cuenta los caracteres en una cadena
-    public int contarCaracteres(String cadena) {
-        // TODO: Implementar el método para contar el número de caracteres en una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
+      ```java
+// Método que cuenta los caracteres en una cadena
+public int contarCaracteres(String cadena) {
+    int cantidad = 0;
 
-        return cadena.length();
+    for (int i = 0; i < cadena.length(); i++) {
+        cantidad++;
     }
 
+    return cantidad;
+}
 
-    // Método que invierte una cadena
-    public String invertirCadena(String cadena) {
-        // TODO: Implementar el método para invertir una cadena.
-        // Ejemplo: Si cadena = "Hello", el resultado debería ser "olleH".
 
-        String resultado = "";
+// Método que invierte una cadena
+public String invertirCadena(String cadena) {
+    String resultado = "";
 
-        for (int i = cadena.length() - 1; i >= 0; i--) {
-            resultado += cadena.charAt(i);
-        }
-
-        return resultado;
+    for (int i = cadena.length() - 1; i >= 0; i--) {
+        resultado += cadena.charAt(i);
     }
+
+    return resultado;
+}
+```
+
 
     // Método que invierte una cadena
     public String invertirCadena(String cadena) {
