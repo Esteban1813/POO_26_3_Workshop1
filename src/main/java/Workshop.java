4a54be986b8c5007ac60c8d3d89b058eab361cec
Juan67 <juanstan08@gmail.com>
2026-09-18 08:41:52 -0500

    cuenta los caracteres en una cadena

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -274,12 +274,14 @@ public int[] rotarArreglo(int[] arreglo, int posiciones) {
     return resultado;
 }
 
-    // Método que cuenta los caracteres en una cadena
+   // Método que cuenta los caracteres en una cadena
     public int contarCaracteres(String cadena) {
         // TODO: Implementar el método para contar el número de caracteres en una cadena.
         // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
-        return 0;
+
+        return cadena.length();
     }
+
 
     // Método que invierte una cadena
     public String invertirCadena(String cadena) {
